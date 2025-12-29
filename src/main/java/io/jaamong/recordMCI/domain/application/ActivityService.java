5152c976ff3e83c4035ebebe0f9ddaf7a4575a0a
jaamong <amypure225@naver.com>
2025-12-29 15:57:21 +0900

    [Feat] ActivityEntity, FoodEntity Service 구현: 하루 첫 접속 초기화 메서드 구현

---
+++ src/main/java/io/jaamong/recordMCI/domain/application/ActivityService.java
@@ -0,0 +1,45 @@
+package io.jaamong.recordMCI.domain.application;
+
+import io.jaamong.recordMCI.domain.entity.ActivityEntity;
+import io.jaamong.recordMCI.domain.entity.ActivityType;
+import io.jaamong.recordMCI.domain.entity.DailyRecordEntity;
+import io.jaamong.recordMCI.domain.repository.ActivityRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+
+    /**
+     * DailyRecord가 오늘 처음 생성될 때 같이 생성되는 디폴트 activity
+     * - walk : 걷기(산책)
+     * - bible transcribe : 성경 필사
+     * - coloring book : 색칠 공부
+     * @return Activity 리스트
+     */
+    @Transactional
+    public List<ActivityEntity> initCreate(DailyRecordEntity dailyRecordEntity) {
+        ActivityEntity walk = ActivityEntity.of(dailyRecordEntity, ActivityType.WALK);
+        ActivityEntity bibleTranscribe = ActivityEntity.of(dailyRecordEntity, ActivityType.BIBLE_TRANSCRIBE);
+        ActivityEntity coloringBook = ActivityEntity.of(dailyRecordEntity, ActivityType.COLORING_BOOK);
+
+        List<ActivityEntity> activities = new ArrayList<>();
+        activities.add(walk);
+        activities.add(bibleTranscribe);
+        activities.add(coloringBook);
+
+        activities = activityRepository.saveAll(activities);
+
+        return activities;
+    }
+}
---
+++ src/main/java/io/jaamong/recordMCI/domain/application/FoodService.java
@@ -0,0 +1,51 @@
+package io.jaamong.recordMCI.domain.application;
+
+import io.jaamong.recordMCI.domain.entity.DailyRecordEntity;
+import io.jaamong.recordMCI.domain.entity.FoodEntity;
+import io.jaamong.recordMCI.domain.entity.NutrientType;
+import io.jaamong.recordMCI.domain.repository.FoodRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class FoodService {
+
+    private final FoodRepository foodRepository;
+
+    /**
+     * DailyRecord가 오늘 처음 생성될 때 같이 생성되는 디폴트 food
+     * - beans : 콩류
+     * - broccoli : 브로콜리
+     * - nuts : 견과류
+     * - yogurt : 요거트
+     * - blueberry : 블루베리
+     * @return Food 리스트
+     */
+    @Transactional
+    public List<FoodEntity> initCreate(DailyRecordEntity dailyRecordEntity) {
+        FoodEntity beans = FoodEntity.of(dailyRecordEntity, NutrientType.PROTEIN, "콩류");
+        FoodEntity broccoli = FoodEntity.of(dailyRecordEntity, NutrientType.CARBO_VEGETABLE, "브로콜리");
+        FoodEntity nuts = FoodEntity.of(dailyRecordEntity, NutrientType.FAT, "견과류");
+        FoodEntity yogurt = FoodEntity.of(dailyRecordEntity, NutrientType.PROTEIN, "요거트");
+        FoodEntity blueberry = FoodEntity.of(dailyRecordEntity, NutrientType.CARBO_FRUIT, "블루베리");
+
+        List<FoodEntity> foodEntities = new ArrayList<>();
+        foodEntities.add(beans);
+        foodEntities.add(broccoli);
+        foodEntities.add(nuts);
+        foodEntities.add(yogurt);
+        foodEntities.add(blueberry);
+
+        foodEntities = foodRepository.saveAll(foodEntities);
+
+        return foodEntities;
+    }
+}
