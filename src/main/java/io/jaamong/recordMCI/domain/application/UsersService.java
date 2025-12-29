8f07eeffc370474a8eea2f44d98e9fb28622862c
jaamong <amypure225@naver.com>
2025-12-29 15:55:45 +0900

    [Feat] UserEntity Service 구현

---
+++ src/main/java/io/jaamong/recordMCI/domain/application/UsersService.java
@@ -0,0 +1,14 @@
+package io.jaamong.recordMCI.domain.application;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UsersService {
+
+}
