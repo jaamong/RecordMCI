8de20ba32bcde1bb4cd127532d4a44469051f3ed
jaamong <amypure225@naver.com>
2025-12-29 17:14:26 +0900

    [Feat] ErrorCode 추가: ActivityEntity 404

--- src/main/java/io/jaamong/recordMCI/api/exception/ErrorCode.java
+++ src/main/java/io/jaamong/recordMCI/api/exception/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
 
     // 4xx
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-    NOT_FOUND_FOOD(HttpStatus.NOT_FOUND, "식품(FoodEntity)을 찾을 수 없습니다.");
+    NOT_FOUND_FOOD(HttpStatus.NOT_FOUND, "식품(FoodEntity)을 찾을 수 없습니다."),
+    NOT_FOUND_ACTIVITY(HttpStatus.NOT_FOUND, "활동(ActivityEntity)를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
