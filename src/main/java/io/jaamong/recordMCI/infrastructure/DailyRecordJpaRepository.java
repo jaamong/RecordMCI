3f257c831ee04419cec7b7858a807cbf012dab76
jaamong <amypure225@naver.com>
2025-12-30 15:25:36 +0900

    [Refactor] (DailyRecordJpaRepository.findDateByUserId) 조회 시 날짜 필터 필드 변경

--- src/main/java/io/jaamong/recordMCI/infrastructure/DailyRecordJpaRepository.java
+++ src/main/java/io/jaamong/recordMCI/infrastructure/DailyRecordJpaRepository.java
@@ -5,16 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface DailyRecordJpaRepository extends JpaRepository<DailyRecordEntity, Long> {
 
     @Query("select dre from DailyRecordEntity dre " +
             "where dre.userEntity.id = :userId AND " +
-            "dre.createdAt >= :startOfToday AND " +
-            "dre.createdAt < :startOfTmr")
-    Optional<DailyRecordEntity> findTodayByUserId(@Param("userId") Long userId,
-                                                  @Param("startOfToday") LocalDateTime startOfToday,
-                                                  @Param("startOfTmr") LocalDateTime startOfTmr);
+            "dre.date = :date")
+    Optional<DailyRecordEntity> findDateByUserId(@Param("userId") Long userId,
+                                                 @Param("date") LocalDate date);
 }
