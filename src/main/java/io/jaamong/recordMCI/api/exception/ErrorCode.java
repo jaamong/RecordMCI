41920c5a895c6db2bd4032e9043b9e0afc0ee04d
jaamong <amypure225@naver.com>
2025-12-31 16:44:16 +0900

    [Feat] ErrorCode 추가: Activity WALK 수정 400

--- src/main/java/io/jaamong/recordMCI/api/exception/ErrorCode.java
+++ src/main/java/io/jaamong/recordMCI/api/exception/ErrorCode.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // 4xx
+    INVALID_ACTIVITY_WALK_UPDATE_REQUEST(HttpStatus.BAD_REQUEST, "걷기(Activity.WALK) 상세 정보를 수정하려면, 걷기 상태가 true 상태여야 합니다."),
+
     NOT_FOUND_RECORD(HttpStatus.NOT_FOUND, "기록(DailyRecord)를 찾을 수 없습니다."),
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
     NOT_FOUND_FOOD(HttpStatus.NOT_FOUND, "식품(FoodEntity)을 찾을 수 없습니다."),
