3edd136f6bc75f63ea46cec4b4ec496b3ee41048
jaamong <amypure225@naver.com>
2026-01-12 21:08:47 +0900

    [Fix] 클라이언트 요청 시 타입불일치 문제 해결

--- src/main/java/io/jaamong/recordMCI/api/dto/request/ActivityWalkUpdateRequest.java
+++ src/main/java/io/jaamong/recordMCI/api/dto/request/ActivityWalkUpdateRequest.java
@@ -6,11 +6,11 @@ import org.hibernate.validator.constraints.Range;
 
 public record ActivityWalkUpdateRequest(
         @Min(value = 0, message = "총 걸음수는 0부터 입력할 수 있습니다.")
-        int totalSteps,
+        Integer totalSteps,
         @Range(min = 0, max = 23, message = "총 시간(hour)은 최소 0, 최대 23까지 입력할 수 있습니다.")
-        int totalHours,
+        Integer totalHours,
         @Range(min = 0, max = 59, message = "총 분(minute)은 최소 0, 최대 59까지 입력할 수 있습니다.")
-        int totalMinutes
+        Integer totalMinutes
 ) {
 
     @Builder
